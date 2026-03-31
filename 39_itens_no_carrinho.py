0e2cafd4eccca3e8bdd621f48459d8574a39421a
Felipe Monteiro <felipe48950467@edu.pa.senac.br>
2026-03-31 20:43:38 -0300

    Cria o desafio itens no carrinho

---
+++ 39_itens_no_carrinho.py
@@ -0,0 +1,4 @@
+'''
+Beatriz está no mercado e quer somar o valor dos itens que colocou no carrinho.
+Objetivo: Desenvolver um programa que pergunte quantos produtos Beatriz quer comprar e o preço dos respectivos produtos produtos, um por um, e ao final mostre o valor total da compra.
+'''
