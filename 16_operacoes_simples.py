a0cfb561e3f9a121dda4a42aeeaa8ac7b4da2ff4
Saulo Bendelak <saulo62504556@edu.pa.senac.br>
2026-04-10 20:56:27 -0300

    correcao_codigo
    Lab 16, correção do código com implementação da função lambda

--- 16_operacoes_simples.py
+++ 16_operacoes_simples.py
@@ -2,29 +2,29 @@
 Objetivo: O sistema deve perguntar ao usuário qual das cinco operações aritméticas (soma, subtração, divisão, multiplicação e exponenciação) deseja realizar com os dois números e exibir os resultados.
 Regra: Use funções para realizar as operações
 '''
-def calculadora (a,b):
-    soma = a + b
-    sub = a - b
-    multi = a * b
-    div = a/b
-    exp = a**b
-    return soma,sub,multi,div,exp
-
-a,b,c,d,e = calculadora(int(input("digite um valor:")),int(input("digite outro valor:")))
-operacao = str(input("Digite uma operação matematica:"))
-
-match operacao:
-    case "soma":
-        print(a)
-    case "sub":
-        print(b)
-    case "mult":
-        print(c)
-    case "div":
-        print(d)
-    case "exp":
-        print(e)
-
-
-
+def calculadora (a,b,operacao):
+    operacoes = {
+        "soma":lambda x,y: x + y,
+        "subtracao":lambda x,y: x-y,
+        "multiplicacao":lambda x,y:x*y,
+        "divisao":lambda x,y:x/y if y!=0 else "Erro, divisão por zero!",
+        "exponencial":lambda x,y:x**y
+    }
+    funcao = operacoes.get(operacao)
+    if funcao:
+        return funcao(a,b)
+    else:
+        return "Operação inválida!"
+while True:
+    num1 = float(input("Digite o primeiro número:"))
+    num2 = float(input("Digite o segundo número:"))
+    op= input("Digite a operação(soma, subtracao, multiplicacao, divisao, exponencial):")
+    resultado = calculadora (num1,num2,op)
+    print("Resultado",resultado)
+    continuar = input("Deseja continuar?(s/n)")
+    if continuar == "s":
+        print("")
+        continue
+    else:
+        break
 
--- 17_o_melhor_preco.py
+++ 17_o_melhor_preco.py
@@ -2,3 +2,5 @@
 Objetivo: Desenvolva um sistema que receba os dois valores, dos amaciantes, informados pelo usuário e indique qual deles é o mais barato.
 Regras: O sistema deve exibir claramente qual dos dois valores é o mais barato. Caso os valores sejam iguais, o sistema deve informar que ambos possuem o mesmo valor. Os valores podem ser inteiros ou decimais.
 '''
+
+
